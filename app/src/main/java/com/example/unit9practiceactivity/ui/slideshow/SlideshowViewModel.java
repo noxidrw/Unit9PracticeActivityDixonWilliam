ca954fc558b82b8bb94941816e98f48ad3cb93af
Bill Dixon <wdixon@css.edu>
2020-03-28 14:29:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/unit9practiceactivity/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.unit9practiceactivity.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}

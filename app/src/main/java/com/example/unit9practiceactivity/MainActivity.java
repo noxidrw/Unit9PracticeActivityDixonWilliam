fbb5b8cf23e14f4848e1e864ceeb7c6397a9d2e8
Bill Dixon <wdixon@css.edu>
2020-03-29 19:04:58 -0500

    Assignment adding the Drawer menu items and functionality.

--- app/src/main/java/com/example/unit9practiceactivity/MainActivity.java
+++ app/src/main/java/com/example/unit9practiceactivity/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.unit9practiceactivity;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -20,9 +20,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -56,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
     @Override
@@ -72,8 +75,8 @@ public class MainActivity extends AppCompatActivity {
 
         //Setup Menu Actions
         if(id == R.id.action_add){
-            Snackbar.make(getWindow().getDecorView(), "Add Study mates not available yet", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+            Toast.makeText(getApplicationContext(), "Add Study mates not available yet", Toast.LENGTH_LONG)
+                    .show();
             return true;
         }if(id == R.id.action_delete){
             Snackbar.make(getWindow().getDecorView(), "Delete Study mates not available yet", Snackbar.LENGTH_LONG)
@@ -101,8 +104,9 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         }
-        return false;
+        return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -110,4 +114,43 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-}
+
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        //Setup Menu Actions
+        if(id == R.id.nav_action_add) {
+            Snackbar.make(getWindow().getDecorView(), "Add Study mates not available yet", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            Toast.makeText(getApplicationContext(), "Add Study mates not available yet", Toast.LENGTH_LONG)
+                    .show();
+            return true;
+        }if(id == R.id.nav_action_delete){
+            Snackbar.make(getWindow().getDecorView(), "Delete Study mates not available yet", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            return true;
+        }if(id == R.id.nav_action_email){
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            emailIntent.setType("*/*");
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hey, did it work?");
+            if(emailIntent.resolveActivity(getPackageManager()) != null){
+                startActivity(emailIntent);
+            }
+            return true;
+        }if(id == R.id.nav_action_settings){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }if(id == R.id.nav_action_sms){
+            Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+            smsIntent.setData(Uri.parse("sms:" + getResources().getString(R.string.smsNum)));
+            smsIntent.putExtra("sms_body", "Hi");
+            // check for SMS APP
+            if(smsIntent.resolveActivity(getPackageManager()) != null){
+                startActivity(smsIntent);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    }
